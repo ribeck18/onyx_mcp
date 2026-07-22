f0590c4c4b84e777562a78c00d5ca98738a37da6
Riley Beck <beckriley515@gmail.com>
2026-07-22 13:24:01 -0600

    Add authenticated API post helper

--- onyx.go
+++ onyx.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -10,7 +11,7 @@ import (
 )
 
 func (c *OnyxClient) apiGet(ctx context.Context, path string, itemId int) (json.RawMessage, error) {
-	endpoint, err := url.JoinPath(c.BaseURL + "api" + "vdi" + strconv.Itoa(itemId))
+	endpoint, err := url.JoinPath(c.BaseURL, "api", path, strconv.Itoa(itemId))
 	if err != nil {
 		return nil, fmt.Errorf("Build url: %w", err)
 	}
@@ -19,6 +20,7 @@ func (c *OnyxClient) apiGet(ctx context.Context, path string, itemId int) (json.
 	if err != nil {
 		return nil, fmt.Errorf("Build request: %w", err)
 	}
+	req.Header.Set("Authorization", "Bearer "+c.PAT)
 
 	client := c.HTTPClient
 	if client == nil {
@@ -38,7 +40,51 @@ func (c *OnyxClient) apiGet(ctx context.Context, path string, itemId int) (json.
 	decoder := json.NewDecoder(resp.Body)
 	decodeErr := decoder.Decode(&jsonResponse)
 	if decodeErr != nil {
-		return nil, fmt.Errorf("Decode response %d: %w", itemId, err)
+		return nil, fmt.Errorf("Decode response %d: %w", itemId, decodeErr)
+	}
+
+	return jsonResponse, nil
+}
+
+func (c *OnyxClient) ApiPost(ctx context.Context, path string, itemId int, payload map[string]string) (json.RawMessage, error) {
+	endpoint, err := url.JoinPath(c.BaseURL, "api", path, strconv.Itoa(itemId))
+	if err != nil {
+		return nil, fmt.Errorf("Build url: %w", err)
+	}
+
+	//convert payload to []byte
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("payload to json: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("Build request: %w", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+c.PAT)
+	req.Header.Add("Content-Type", "application/json")
+
+	client := c.HTTPClient
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("post method response: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
+		return nil, fmt.Errorf("get item %d: %s", itemId, resp.Status)
+	}
+
+	var jsonResponse json.RawMessage
+	decoder := json.NewDecoder(resp.Body)
+	decodeErr := decoder.Decode(&jsonResponse)
+	if decodeErr != nil {
+		return nil, fmt.Errorf("JSON decode: %w", decodeErr)
 	}
 
 	return jsonResponse, nil
--- project.go
+++ project.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (c *OnyxClient) getProject(ctx context.Context, projectId int) (json.RawMessage, error) {
-	project, err := c.apiGet(ctx, "vdi", projectId)
+	project, err := c.apiGet(ctx, "project", projectId)
 	if err != nil {
 		return nil, fmt.Errorf("Error getting project: %w", err)
 	}
